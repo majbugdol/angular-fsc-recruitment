15c9472c11bc3e7bb234de75eb4b12bfa5e51539
majbugdol <majbugdol@gmail.com>
2022-09-13 21:57:58 +0200

    add person name

--- src/app/work-orders-page/table/table.component.ts
+++ src/app/work-orders-page/table/table.component.ts
@@ -19,6 +19,18 @@ export class TableComponent implements OnChanges {
     'priority',
   ];
 
+  displayAssignedTo(element: IWorkOrder): string {
+    let assignedToString = '';
+    if (element.assigned_to.length) {
+      if (element.assigned_to.length === 1) {
+        return (assignedToString = element.assigned_to[0].person_name);
+      }
+      for (let i = 0; i < element.assigned_to.length; i++) {
+        assignedToString = `${element.assigned_to[i].person_name}   ${assignedToString}`;
+      }
+      return assignedToString;
+    } else return '-';
+  }
   public dataSource: IWorkOrder[];
 
   constructor() {}
