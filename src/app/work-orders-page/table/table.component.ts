a08517125d4971d3e213b23c3f48277c75361775
majbugdol <majbugdol@gmail.com>
2022-09-13 21:32:37 +0200

    add table

--- src/app/work-orders-page/table/table.component.ts
+++ src/app/work-orders-page/table/table.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { IWorkOrder } from 'src/app/services/workload.service';
-import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
 })
-export class TableComponent implements OnInit {
-  @Input() workOrders: IWorkOrder[];
+export class TableComponent implements OnChanges {
+  @Input() workOrders: IWorkOrder[] = [];
+  // dataSource2 = new MatTableDataSource(table);
+
+  public displayedColumns: string[] = [
+    'work_order_id',
+    'description',
+    'received_date',
+    'assigned_to',
+    'status',
+    'priority',
+  ];
+
+  public dataSource: IWorkOrder[];
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnChanges(): void {
+    this.dataSource = this.workOrders;
+  }
 }
